69d021ec3767fa498dbaff06104c92133a65c938
Anthony Catan-Cavery <a.catancavery@gmail.com>
2025-11-30 15:34:06 +0100

    mon premier -component-

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { bindCallback } from 'rxjs';
+import { PlayingCard } from './components/playing-card/playing-card';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [PlayingCard],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class App {
-  protected readonly title = signal('playing-cards');
+
 }
